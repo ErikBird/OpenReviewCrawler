b24978bb823ffd72a3d23e0f94c9b7fde093a5e8
erikschwan <github@erikschwan.de>
2019-11-05 21:39:31 +0100

    I added a function to access the revision information. To implement the functionality efficiently, I changed the architecture slightly and improved the loggings.

--- crawler.py
+++ crawler.py
@@ -5,32 +5,57 @@ from datetime import date
 import re
 import openreview
 import requests
-
+import logging
+import sys
+import progressbar
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import selenium.webdriver.support.ui as ui
+import time
 
-def crawl(client, config):
+def crawl(client, config,log):
     results = []
+    driver  = start_webdriver()
     for target in config["targets"]:
         venue, years = target["venue"], target["years"]
         if years == "all":
             years = range(2000, date.today().year + 2)
         for year in years:
+            log.info('Current Download:'+ venue+' in '+str(year))
             invitations_iterator = openreview.tools.iterget_invitations(client, regex="{}/{}/".format(venue, year))
             invitations = [inv.id for inv in invitations_iterator]
             invitations = merge_invitations(invitations)
             if not invitations:
+                log.warning('No Data for'+ venue+' in '+str(year))
                 continue
             else:
                 invs = []
-                for inv in invitations:
+                for inv in progressbar.progressbar(invitations):
+                    notes = [note for note in openreview.tools.iterget_notes(client, invitation=inv)]
+                    revisions = []
+                    for n in notes:
+                        revisions.append(get_revisions(n.id,driver))
+
                     invs.append({"invitation": inv,
-                                        "notes": [note.to_json() for note in openreview.tools.iterget_notes(client, invitation=inv)]})
+                                        "notes": [note.to_json() for note in notes],
+                                 'revisions': [json.dumps(rev) for  rev in revisions if revisions]})
                 results.append({"venue": venue, "year": year, "invitations": invs})
 
     with open(os.path.join(config["outdir"], config["filename"]), 'w') as file_handle:
         json.dump(results, file_handle, indent=config["json_indent"])
+
+
+def start_webdriver():
+    options = Options()
+    options.headless = True
+    try:
+        driver = webdriver.Firefox(options=options,
+                                   executable_path=config['geckodriver'])
+    except:
+        print(
+            'The webdriver has not been setup correctly! Please follow the README Installation instructions to setup the geckodriver.')
+    return driver
+
 
 def merge_invitations(invitations):
     new_invitations = set()
@@ -40,8 +65,39 @@ def merge_invitations(invitations):
         new_invitations.add(sub2)
     return new_invitations
 
+def get_revisions(id,driver):
+    my_url = 'http://openreview.net/revisions?id='+id
+    driver.get(my_url)
+
+    wait = ui.WebDriverWait(driver, 10)
+    try:
+        wait.until(lambda driver: driver.find_element_by_class_name('note_content_pdf'))
+    except:
+        log.error(id+' id causes a timeout')
+
+    revisions = []
+    for revision in driver.find_elements_by_class_name('note'):
+        title = revision.find_element_by_class_name('note_content_title').text
+        try:
+            pdf = revision.find_element_by_class_name('note_content_pdf').get_attribute('href')
+        except:
+            pdf = 'NoPDF'
+            log.error(id + ' provides no PDF')
+            pass
+        authors = [p.text for p in revision.find_elements_by_class_name('profile-link')]
+        date = revision.find_element_by_class_name('date').text
+        notes = [p.text for p in revision.find_elements_by_class_name('note_contents')]
+        revisions.append({'title': title, 'pdf': pdf, 'authors': authors, 'date': date, 'pdf': pdf, 'notes': notes})
+
+    return revisions
+
+
 
 if __name__ == '__main__':
+    log = logging.getLogger("crawler")
+    log.setLevel(logging.INFO)
+    progressbar.streams.wrap_stderr()
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-c', '--config', help='configuration for the crawling', default='config.json')
@@ -50,89 +106,30 @@ if __name__ == '__main__':
     parser.add_argument('--baseurl', default='https://openreview.net')
 
     args = parser.parse_args()
-
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
     try:
         config = json.load(open(args.config))
     except:
         print('The configuration File has not been found. \n Please Make sure it is correctly Named \'config.json\' and is located in the project root foulder.\n Otherwise please specify the configuration Path with the parser argument \'-c PATH\'')
 
+    '''
     print("Available venues:")
     c = openreview.Client(baseurl='https://openreview.net')
     venues = openreview.tools.get_all_venues(c)
     print(*venues, sep="\n")
-
-    my_url = 'http://openreview.net/revisions?id=ryQu7f-RZ'
-    options = Options()
-    options.headless = True
-    try:
-        driver = webdriver.Firefox(options=options,
-                                   executable_path=config['geckodriver'])
-    except:
-        print('The webdriver has not been setup correctly! Please follow the README Installation instructions to setup the geckodriver.')
-
-
-    # driver = webdriver.PhantomJS('Resources/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
-    driver.get(my_url)
-    wait = ui.WebDriverWait(driver, 10)
-    wait.until(lambda driver: driver.find_element_by_class_name('row'))
-    print(driver.find_element_by_class_name('row'))
-
-    for elm in driver.find_elements_by_class_name('title_pdf_row clearfix'):
-        print(elm.text)
-
-
     '''
-    if args.config is None:
 
-        print("Available venues:")
-        c = openreview.Client(baseurl='https://openreview.net')
-        venues = openreview.tools.get_all_venues(c)
-        print(*venues, sep="\n")
-
-        my_url = 'http://openreview.net/revisions?id=ryQu7f-RZ'
-        options = Options()
-        options.headless = True
-        driver = webdriver.Firefox(options=options, executable_path=r'/home/erikschwan/PycharmProjects/OpenReviewCrawler/geckodriver')
-        #driver = webdriver.PhantomJS('Resources/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
-        driver.get(my_url)
-        wait = ui.WebDriverWait(driver, 10)
-        wait.until(lambda driver: driver.find_element_by_class_name('row'))
-        print(driver.find_element_by_class_name('row'))
-        for elm in driver.find_elements_by_class_name('title_pdf_row clearfix'):
-           print(elm.text)
-        #response = session.body()
-        #soup = BeautifulSoup(response)
-        #print(soup.find(id="note_Syax5XD9M"))
-        #response = c.__handle_response(response)
-        #for t in response.json()['tags']:
-        #   print(t)
-
+    username = config["username"]
+    if args.password is not None:
+        password = args.password
     else:
-        config = json.load(open(args.config))
-        my_url = 'http://openreview.net/revisions?id=ryQu7f-RZ'
-        options = Options()
-        options.headless = True
-        driver = webdriver.Firefox(options=options,
-                                   executable_path=config['geckodriver'])
-        # driver = webdriver.PhantomJS('Resources/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
-        driver.get(my_url)
-        wait = ui.WebDriverWait(driver, 10)
-        wait.until(lambda driver: driver.find_element_by_class_name('row'))
-        print(driver.find_element_by_class_name('row'))
+        password = config["password"]
 
-        for elm in driver.find_elements_by_class_name('title_pdf_row clearfix'):
-            print(elm.text)
+    client = openreview.Client(
+        baseurl='https://openreview.net',
+        username=username,
+        password=password)
+    log.info('Login as '+username+' was successful')
 
-        username = config["username"]
-        if args.password is not None:
-            password = args.password
-        else:
-            password = config["password"]
+    crawl(client, config,log)
 
-        client = openreview.Client(
-            baseurl='https://openreview.net',
-            username=username,
-            password=password)
-
-        crawl(client, config)
-        '''
