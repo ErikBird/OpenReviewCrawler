44071d8c694d15201bf6c64b7ea92171ca1e0f04
Aaronsom <gregor.geigle@gmail.com>
2019-11-10 08:51:35 +0100

    added parsing of raw data into format for group 2b (grouped via submissions) added revision of comments

--- crawler.py
+++ crawler.py
@@ -13,33 +13,52 @@ from selenium.webdriver.firefox.options import Options
 import selenium.webdriver.support.ui as ui
 import time
 
-def crawl(client, config,log):
+def crawl(client, config, log):
     results = []
-    driver  = start_webdriver()
+    driver = start_webdriver()
     for target in config["targets"]:
         venue, years = target["venue"], target["years"]
         if years == "all":
             years = range(2000, date.today().year + 2)
         for year in years:
-            log.info('Current Download:'+ venue+' in '+str(year))
+            log.info('Current Download: '+ venue+' in '+str(year))
             invitations_iterator = openreview.tools.iterget_invitations(client, regex="{}/{}/".format(venue, year))
             invitations = [inv.id for inv in invitations_iterator]
             invitations = merge_invitations(invitations)
             if not invitations:
-                log.warning('No Data for'+ venue+' in '+str(year))
+                log.warning('No data for'+ venue+' in '+str(year))
                 continue
             else:
-                invs = []
+                submissions = []
+                forum_idx_map = {}
+                other_notes = []
                 for inv in progressbar.progressbar(invitations):
-                    notes = [note for note in openreview.tools.iterget_notes(client, invitation=inv)]
-                    revisions = []
-                    for n in notes:
-                        revisions.append(get_revisions(n.id,driver))
-
-                    invs.append({"invitation": inv,
-                                        "notes": [note.to_json() for note in notes],
-                                 'revisions': [json.dumps(rev) for  rev in revisions if revisions]})
-                results.append({"venue": venue, "year": year, "invitations": invs})
+                    notes = [note.to_json() for note in openreview.tools.iterget_notes(client, invitation=inv)]
+                    if "ubmission" in inv:
+                        # this is a bit of a hack but there is no general submission invitation but all submission invitations
+                        # contain at least (S|s)ubmission somewhere
+                        # see https://openreview-py.readthedocs.io/en/latest/get_submission_invitations.html to verify
+                        log.info("Submission invitation")
+                        forum_idx_map.update({n["forum"]: i+len(submissions) for i, n in enumerate(notes)})
+                        for n in notes:
+                            n["revisions"] = get_submission_revisions(n["id"], driver)
+                            n["notes"] = []
+                        submissions.extend(notes)
+                    else:
+                        revisions = [r.to_json() for r in client.get_references(invitation=inv)]
+                        for note in notes:
+                            note["revisions"] = [r for r in revisions if r["referent"] == note["id"]]
+                        other_notes.extend(notes)
+                if not submissions:
+                    log.warning('No submissions found for '+ venue+' in '+str(year))
+                    continue
+                else:
+                    for note in other_notes:
+                        try:
+                            submissions[forum_idx_map[note["forum"]]]["notes"].append(note)
+                        except KeyError:
+                            log.info("No submission found for note "+note["id"]+" in forum "+note["forum"])
+                results.append({"venue": venue, "year": year, "submissions": submissions})
 
     with open(os.path.join(config["outdir"], config["filename"]), 'w') as file_handle:
         json.dump(results, file_handle, indent=config["json_indent"])
@@ -65,7 +84,8 @@ def merge_invitations(invitations):
         new_invitations.add(sub2)
     return new_invitations
 
-def get_revisions(id,driver):
+
+def get_submission_revisions(id, driver):
     my_url = 'http://openreview.net/revisions?id='+id
     driver.get(my_url)
 
@@ -90,7 +110,6 @@ def get_revisions(id,driver):
         revisions.append({'title': title, 'pdf': pdf, 'authors': authors, 'date': date, 'pdf': pdf, 'notes': notes})
 
     return revisions
-
 
 
 if __name__ == '__main__':
